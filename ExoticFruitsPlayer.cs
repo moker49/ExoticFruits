9ced89e278714e6694111d5883b977a4870d80e5
moker49 <moker49@gmail.com>
2022-07-20 14:30:33 -0400

    fixes

--- ExoticFruitsPlayer.cs
+++ ExoticFruitsPlayer.cs
@@ -5,37 +5,37 @@ using ExoticFruits.Items;
 
 namespace ExoticFruits
 {
-	public class ExoticFruitsPlayer : ModPlayer
-	{
-		public byte[] exoticFruits = new byte[10];
+    public class ExoticFruitsPlayer : ModPlayer
+    {
+        public byte[] exoticFruits = new byte[10];
 
-		public override void ResetEffects()
-		{
-			foreach (byte item in exoticFruits)
+        public override void ResetEffects()
+        {
+            foreach (byte item in exoticFruits)
             {
-				Player.statLifeMax2 += item * ExoticFruitsItem.LifePerFruit;
-				Player.statManaMax2 += item * ExoticFruitsItem.ManaPerFruit;
-			}
+                Player.statLifeMax2 += item * ExoticFruitsFruit.LifePerFruit;
+                Player.statManaMax2 += item * ExoticFruitsFruit.ManaPerFruit;
+            }
 
-		}
+        }
 
-		public override void SyncPlayer(int toWho, int fromWho, bool newPlayer)
-		{
-			ModPacket packet = Mod.GetPacket();
-			//packet.Write((byte)ExampleMod.MessageType.ExamplePlayerSyncPlayer);
-			packet.Write((byte)Player.whoAmI);
-			packet.Write(exoticFruits);
-			packet.Send(toWho, fromWho);
-		}
+        public override void SyncPlayer(int toWho, int fromWho, bool newPlayer)
+        {
+            ModPacket packet = Mod.GetPacket();
+            //packet.Write((byte)ExampleMod.MessageType.ExamplePlayerSyncPlayer);
+            packet.Write((byte)Player.whoAmI);
+            packet.Write(exoticFruits);
+            packet.Send(toWho, fromWho);
+        }
 
-		public override void SaveData(TagCompound tag)
-		{
-			tag["ExoticFruitss"] = exoticFruits;
-		}
+        public override void SaveData(TagCompound tag)
+        {
+            tag["ExoticFruitss"] = exoticFruits;
+        }
 
-		public override void LoadData(TagCompound tag)
-		{
-			exoticFruits = (byte[])tag["ExoticFruitss"];
-		}
-	}
+        public override void LoadData(TagCompound tag)
+        {
+            exoticFruits = (byte[])tag["ExoticFruitss"];
+        }
+    }
 }
